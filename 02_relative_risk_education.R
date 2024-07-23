2edcc6865959233f12d4b4b22e826182ec25e630
Rich-P-Martin <richard.martin@gov.bc.ca>
2024-07-22 18:50:41 -0700

    some changes

--- 02_relative_risk_education.R
+++ 02_relative_risk_education.R
@@ -8,30 +8,30 @@ conflicts_prefer(dplyr::filter)
 #constants------------
 cut_off=.2
 #functions---------------
-get_relative_risk <- function(edu, tbbl){
-  prob_bad_conditional_on_education <- tbbl|>
+get_relative_risk <- function(edu){
+  prob_lsls_conditional_on_education <- joined|>
     filter(Education==edu)|>
     group_by(occ_type)|>
     summarize(value=sum(value))|>
     ungroup()|>
     mutate(value=value/sum(value))|>
-    filter(occ_type=="bad")|>
+    filter(occ_type=="LSLS")|>
     pull(value)
 
-  prob_bad_conditional_on_not_education <- tbbl|>
+  prob_lsls_conditional_on_not_education <- joined|>
     filter(Education!=edu)|>
     group_by(occ_type)|>
     summarize(value=sum(value))|>
     ungroup()|>
     mutate(value=value/sum(value))|>
-    filter(occ_type=="bad")|>
+    filter(occ_type=="LSLS")|>
     pull(value)
 
-  relative_risk <- prob_bad_conditional_on_education/prob_bad_conditional_on_not_education
+  relative_risk <- prob_lsls_conditional_on_education/prob_lsls_conditional_on_not_education
 }
 
 #the code--------------------
-cip_noc_all <- read_csv(here("out","cip_noc_all_ages_processed.csv"))|>
+cip_noc_all <- read_csv(here("out","cip_noc_all_ages.csv"))|>
   rename(NOCtemp=NOC)|>
   mutate(NOC=str_sub(NOCtemp, 1,5),
          Description=str_sub(NOCtemp, 7), .before=everything())|>
@@ -43,10 +43,6 @@ noc_names <- cip_noc_all|>
 
 median_salaries <- read_excel(here("data","median_salary.xlsx"))|>
   select(NOC, median_salary=contains("calculated"))
-
-paths <- read_excel(here("out","top_paths_to_and_from.xlsx"), sheet = "Paths from Education all ages")|>
-  select(highest=`For students who attained`,CIP=`in this field of study`, paths=`# of educations with share > 1%`)|>
-  distinct()
 
 skills <- read_excel(here("data", "skills_original.xlsx"))|>
   mutate(NOC=str_pad(noc2021, 5, "left", "0"), .before=everything())|>
@@ -60,20 +56,39 @@ low_median_salary <- median_salaries|>
 low_skills <- skills|>
   slice_min(average_skill, prop = cut_off)
 
-bad_occ <- inner_join(low_median_salary, low_skills)|>
-  mutate(occ_type="bad")|>
+lsls_occ <- inner_join(low_median_salary, low_skills)|>
+  mutate(occ_type="LSLS")|>
   left_join(noc_names)
 
-joined <- full_join(cip_noc_all, bad_occ)|>
-  mutate(occ_type=if_else(is.na(occ_type), "good",  occ_type))
+joined <- full_join(cip_noc_all, lsls_occ)|>
+  mutate(occ_type=if_else(is.na(occ_type), "not",  occ_type))
 
-nested <- tibble(Education=unique(joined$Education), data=list(joined))|>
+nested <- tibble(Education=unique(joined$Education))|>
   na.omit()|>
-  mutate(relative_risk=map2_dbl(Education, data, get_relative_risk))
+  mutate(relative_risk=map_dbl(Education, get_relative_risk))
 
 relative_risk <- nested|>
   select(Education, relative_risk)|>
   separate(Education, into=c("CIP","highest"), sep=": ")
 
-write_csv(bad_occ, here("out","bad_occ.csv"))
+write_csv(lsls_occ, here("out","lsls_occ.csv"))
 write_csv(relative_risk, here("out","relative_risk.csv"))
+
+#sociology stuff-------------------------------
+
+lsls_occ <- lsls_occ|>
+  select(NOC, occ_type)
+
+joined2 <- full_join(cip_noc_all, skills)|>
+  full_join(median_salaries)|>
+  filter(Education=="11.99 Computer and information sciences and support services, other: Bachelor's degree" |
+           Education=="45.13 Sociology and anthropology: Bachelor's degree")|>
+  filter(value>0)|>
+  na.omit()|>
+  left_join(lsls_occ, by="NOC")|>
+  mutate(occ_type=if_else(is.na(occ_type),"not",occ_type))
+
+
+write_csv(joined2, here("out","joined2.csv"))
+
+
